2910a9e025d7b8a8c810c6ab4e4469321d433a00
medinar <rommel.d.medina@gmail.com>
2020-03-25 18:26:18 +0800

    Initial commit.

---
+++ src/main/java/com/medinar/spring/springsecurityjdbc/SpringSecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.medinar.spring.springsecurityjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcApplication.class, args);
+	}
+
+}
